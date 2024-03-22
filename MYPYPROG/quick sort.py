c2511e6e744833c000d72b315997b964be7f11c4
Dipto2611 <diptaraj.ch.26@gmail.com>
2024-03-22 13:03:57 +0530

    again

--- MYPYPROG/quick sort.py
+++ MYPYPROG/quick sort.py
@@ -7,7 +7,7 @@ def quicksort(seq):
         pivot=seq.pop()  #mark the last item as center
 
 
-    left_items=[]
+    left_items=[] 
     right_items=[]
     
     for items in seq:
@@ -17,8 +17,10 @@ def quicksort(seq):
             right_items.append(items)
 
     
-    return quicksort(left_items)+[pivot]+quicksort(right_items)
+    return quicksort(left_items) + [pivot] + quicksort(right_items) #recursion
 
-seq=[27,9,6,78,20,5,14,31,5]
+seq=[27,1,3,9,6,78,20,5,14]
+
 sorted_seq = quicksort(seq)
 print("Sorted sequence is:", sorted_seq)
+
